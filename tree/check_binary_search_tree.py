135bf1ff323daf24ed4627da0570290c92cca001
Linh Bui <mail@linhbui.me>
2015-10-19 23:56:48 -0700

    check if binary search tree

--- tree/check_binary_search_tree.py
+++ tree/check_binary_search_tree.py
@@ -5,12 +5,13 @@ class Node:
         right = None
 
     def check_binary_search_tree(node):
+        return check_binary_search_tree_helper(node, float("-inf"), float("inf"))
+
+    def check_binary_search_tree_helper(node, min_val, max_val): 
         if node == None:
             return True
-        if node.left != None && node.left > node.val 
+        if node.val < min_val || node.val > max_val:
             return False
-        if node.right != None && node.right < node.val 
-            return False
-        left = check_binary_search_tree(node.left)
-        right = check_binary_search_tree(node.right)
+        left = check_binary_search_tree_helper(node.left, min_val, node.val)
+        right = check_binary_search_tree_helper(node.right, node.val, max_val)
         return left && right
